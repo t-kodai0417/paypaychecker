be545f326bd67b001a4c60bf0f8789c0dab5bbe0
t-Kodai <75510055+t-kodai0417@users.noreply.github.com>
2022-06-20 21:49:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import paybase
+import paybase,time
 print("PayPayLinkChecker\nDeveloped by Kodai.")
 txt_name=input("チェックするリンクが入ったtxtファイルを指定してください。\n拡張子まで入力してください。"+"\n>>")
 try:
@@ -9,6 +9,12 @@ except:
   print("読み込めませんでした。")
   exit()
 hozon=input("workedなリンクの保存先を入力してください。\n拡張子は入力しなくてよいです。\n>>")
+delay1=input("Delayを入力してください。\nおすすめは3です。0にするとIPが死ぬかも？\n>>")
+try:
+  delay1=int(delay1)
+except:
+  print("Delayでは数字を指定してください。")
+  exit()
 for i in paylink:
   paybase.check(i,hozon)
 print("終了")
