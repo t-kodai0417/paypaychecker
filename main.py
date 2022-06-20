4c370a537a354e2a230810c1a1c53dc63c21e80f
t-Kodai <75510055+t-kodai0417@users.noreply.github.com>
2022-06-20 20:30:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-import base
+import paybase
+txt_name=input("チェックするリンクが入ったtxtファイルを指定してください。\n拡張子まで入力してください。"+"\n>>")
+try:
+  f=open(txt_name,"r")
+  paylink=f.read().split('\n')
+  f.close()
+except:
+  print("読み込めませんでした。")
+  exit()
