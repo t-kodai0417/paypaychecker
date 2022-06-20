8274b3ae8de17e811e1c1b6ccc6029ef10738abb
t-Kodai <75510055+t-kodai0417@users.noreply.github.com>
2022-06-20 20:24:39 +0900

    Create base.py

--- base.py
+++ base.py
@@ -1,11 +1,6 @@
 import requests,os,json,uuid
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
-    'Accept' : 'application/json, text/plain, */*',
-    'Content-Type' : 'application/json'
-    }
 
-def check_price(code):
+def check_price(code,filea):
     code=code.replace("https://pay.paypay.ne.jp/","")
     client_uuid=str(uuid.uuid4())
     #リンクの情報を取得
@@ -15,10 +10,25 @@ def check_price(code):
     }
     aaaaaa=requests.get(f"https://www.paypay.ne.jp/app/v2/p2p-api/getP2PLinkInfo?verificationCode={code}&client_uuid={client_uuid}",headers=getp2pinfo)
     dataf=aaaaaa.json()
-    with open('test.json', 'w') as f:
-        json.dump(dataf, f, ensure_ascii=False, indent=4)
-    if dataf["payload"]["orderStatus"]=="PENDING":
-        print(f"[+]https://pay.paypay.ne.jp/{code}")
+    try:
+        if dataf["payload"]["orderStatus"]=="PENDING":
+            print(f"[+]https://pay.paypay.ne.jp/{code}")
+            f = open(f"{filea}.txt", 'a', encoding='utf-8')
+            aaa=f.read()
+            f.close()
+            f = open(f"{filea}.txt", 'a', encoding='utf-8')
+            if aaa=="":
+                f.write(f"https://pay.paypay.ne.jp/{code}")
+            else:
+                f.write('\n'+f"https://pay.paypay.ne.jp/{code}")
+            f.close()
+            return(f"success")
+        else:
+            print(f"[-]https://pay.paypay.ne.jp/{code}  NoPending")
+            return("died")
+    except:
+        print(f"[-]https://pay.paypay.ne.jp/{code}  NotExist")
+        return("died")
     #値段 dataf["payload"]["pendingP2PInfo"]["amount"]
     #orderStatus dataf["payload"]["orderStatus"] PENDING
     
