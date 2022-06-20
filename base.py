293d09d28b9b842bd4df45cc556658e9be359020
t-Kodai <75510055+t-kodai0417@users.noreply.github.com>
2022-06-20 19:59:01 +0900

    Update base.py

--- base.py
+++ base.py
@@ -9,8 +9,8 @@ def check_price(code):
     client_uuid=str(uuid.uuid4())
     #リンクの情報を取得
     getp2pinfo={
-    "Accept":"application/json, text/plain, */*",
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'
+        "Accept":"application/json, text/plain, */*",
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'
     }
     aaaaaa=requests.get(f"https://www.paypay.ne.jp/app/v2/p2p-api/getP2PLinkInfo?verificationCode={code}&client_uuid={client_uuid}",headers=getp2pinfo)
     dataf=aaaaaa.json()
